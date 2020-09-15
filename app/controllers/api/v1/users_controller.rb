12a9b152886bf07614fb076d717e40e98497bae9
Hasan Karlı <karlihasann@gmail.com>
2020-09-16 02:08:16 +0300

    add crud functions inside controller and routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,73 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :getUser, only: [:updateUser, :deleteUser, :showUser]
+
   def getUsers
+    users = User.all
+    if users
+      render json: users, status: :ok
+    else
+      render json: {msg: "Users not found"}, status: :unprocessable_entity
+    end
 
   end
 
   def addUser
-    user = User.new(username: params[:username], email: params[:email], password_digest: params[:password_digest])
+    user = User.new(userparams)
     
     if user.save()
       render json: user, status: :ok
     
     else 
-      render json: {message: "User not added"}, status: :unprocessable_entity
+      render json: {msg: "User not added"}, status: :unprocessable_entity
     end
 
   end
+
+  def showUser
+    if @user
+        render json: @user,status: :ok      
+    else
+      render json: { msg: "User not found" }, status: :unprocessable_entity
+    end
+  end
   
+
+  def updateUser
+    if @user
+      if @user.update(userparams)
+        render json: @user,status: :ok
+      else
+        render json: { msg: "User not updated" }, status: :unprocessable_entity
+      end
+      
+    else
+      render json: { msg: "User not found" }, status: :unprocessable_entity
+    end
+  end
+
+  def deleteUser
+    if @user
+      if @user.destroy()
+        render json: @user,status: :ok
+      else
+        render json: { msg: "User not deleted" }, status: :unprocessable_entity
+      end
+      
+    else
+      render json: { msg: "User not found" }, status: :unprocessable_entity
+    end
+  end
+  
+  
+
+  private
+    def userparams
+      params.permit(:username, :email, :password_digest)
+    end
+    
+    def getUser
+      @user = User.find(params[:id])
+    end
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'get_users', action: :getUsers, controller: :users
+      get 'show_user', action: :showUser, controller: :users
       post 'add_user', action: :addUser, controller: :users
+      put 'update_user', action: :updateUser, controller: :users
+      delete 'delete_user', action: :deleteUser, controller: :users
     end
   end
 end
