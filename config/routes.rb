3c1c5df3c521aab95b292e5adfbc4db0256a94a9
Hasan Karlı <karlihasann@gmail.com>
2020-09-16 10:57:48 +0300

    login api for authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    namespace :auth do
+      post 'login', action: :login, controller: :auth
+    end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     namespace :v1 do
       get 'get_users', action: :getUsers, controller: :users
       get 'show_user', action: :showUser, controller: :users
